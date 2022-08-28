070ae599a0d2c83bc1c6fa26e1006dfc2fbecf7d
akoub <akoubensky@gmail.com>
2022-08-28 17:16:58 +0300

    Restore

--- Lesson1/src/Guess.java
+++ Lesson1/src/Guess.java
@@ -10,19 +10,6 @@ public class Guess {
     }
 
     public static void main(String[] args) {
-        int a = 1, b = 100;
-        do {
-            int m = (a+b)/2;
-            // a <= m < b
-            int r = memory(m);
-            if (r < 0) {
-                // Середина меньше задуманного
-                a = m + 1;
-            } else {
-                // Середина больше или равна задуманного
-                b = m;
-            }
-        } while (a < b);
-        System.out.println("Guess = " + a + ", answer = " + answer);
+        // Здесь будет Ваша реализация
     }
 }
