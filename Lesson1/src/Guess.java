e53583dadd624a935c408a6746ac83f0cee3cd86
Dmitry <PopovDN93@gmail.com>
2022-09-03 20:20:54 +0700

    Изменение от 03.09

--- Lesson1/src/Guess.java
+++ Lesson1/src/Guess.java
@@ -11,5 +11,32 @@ public class Guess {
 
     public static void main(String[] args) {
         // Здесь будет Ваша реализация
+        for (int i =0;i<10000;i++) {
+            answer=0;
+
+            int a = 1;
+            int b = 100;
+            int y;
+            do {
+                y = (b + a) / 2;
+//                System.out.println(y);
+                if (memory(y) == 1) {
+                    b = y;
+//                    System.out.println("b= " + b);
+                } else {
+                    if (memory(y) == -1) {
+                        a = y+1;
+//                        System.out.println("a= " + a);
+                    }
+                }
+            } while (memory(y) != 0);
+            System.out.println(y);
+            System.out.println(answer);
+        }
+//        for (int y =0;y<=100;y++) {
+//            System.out.println("y= "+y+" значение функции "+memory('y'));
+//
+//            if (memory('y')==0) break;
+//        }
     }
 }
