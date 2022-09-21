1d2537bf96205a9bee56b7b896ff9a5f178c8bd0
akoub <akoubensky@gmail.com>
2022-09-21 20:34:08 +0300

    Life

---
+++ Life/src/Field.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Field {
+    private enum State {
+        EMPTY, FILLED
+    }
+    private State[][] field;
+
+    public static void main(String[] args) throws IOException {
+        List<String> list =  Files.readAllLines(Path.of("Life", "Glyder"));
+    }
+}
