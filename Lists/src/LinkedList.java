ab35b5895832852b97bd7c978edaf44c46404d4f
akoub <akoubensky@gmail.com>
2022-09-28 20:27:22 +0300

    LinkedList

--- Lists/src/LinkedList.java
+++ Lists/src/LinkedList.java
@@ -1,48 +1,110 @@
-public class LinkedList {
-    private static class Link {
-        int info;
-        Link next;
+import java.util.Iterator;
 
-        Link(int info) {
+public class LinkedList<E> implements Iterable<E> {
+    @Override
+    public Iterator<E> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<E> {
+        Link<E> current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public E next() {
+            E result = current.getInfo();
+            current = current.next;
+            return result;
+        }
+    }
+
+    private static class Link<T> {
+        T info;
+        Link<T> next;
+
+        Link(T info) {
             this.info = info;
             this.next = null;
         }
 
-        Link(int info, Link next) {
+        Link(T info, Link<T> next) {
             this.info = info;
             this.next = next;
         }
 
-        int getInfo() {
+        T getInfo() {
             return info;
         }
 
-        Link getNext() {
+        Link<T> getNext() {
             return next;
         }
     }
 
-    private Link first;
+    private Link<E> first;
+    private int count = 0;
 
     public LinkedList() {
         first = null;
     }
 
-    public void add(int n) {
-        Link newLink = new Link(n, first);
+    public void add(E n) {
+        Link<E> newLink = new Link<>(n, first);
         first = newLink;
+        count++;
+    }
+
+    public void remove(E toRemove) {
+        if (first != null && first.info.equals(toRemove)) {
+            remove();
+        } else {
+            for (Link<E> current = first; current.next != null; current = current.next) {
+                if (current.next.info == toRemove) {
+                    current.next = current.next.next;
+                    count--;
+                    return;
+                }
+            }
+        }
+    }
+
+    public void removeRec(E toRemove) {
+        if (first != null) removeRec(toRemove, first);
+    }
+
+    private void removeRec(E toRemove, Link<E> current) {
+        if (current.next == null) return;
+        if (current.next.info.equals(toRemove)) {
+            current.next = current.next.next;
+            count--;
+        } else {
+            removeRec(toRemove, current.next);
+        }
+    }
+
+
+    public E remove() {
+        if (first == null) throw new IllegalStateException("Пустой список");
+        E result = first.info;
+        first = first.next;
+        count--;
+        return result;
     }
 
     public int size() {
-        int count = 0;
-//        Link current = first;
-//        while (current != null) {
+//        int count = 0;
+////        Link current = first;
+////        while (current != null) {
+////            count++;
+////            current = current.getNext();
+////        }
+//        for(Link current = first; current != null; current = current.getNext()) {
 //            count++;
-//            current = current.getNext();
 //        }
-        for(Link current = first; current != null; current = current.getNext()) {
-            count++;
-        }
         return count;
     }
 
@@ -50,24 +112,24 @@ public class LinkedList {
         return sizeRecRec(first);
     }
 
-    private int sizeRecRec(Link link) {
+    private int sizeRecRec(Link<E> link) {
         return link == null ? 0 : 1 + sizeRecRec(link.next);
     }
 
-    public int sum() {
-        int result = 0;
-        for(Link current = first; current != null; current = current.getNext()) {
-            result += current.getInfo();
-        }
-        return result;
-    }
+//    public int sum() {
+//        int result = 0;
+//        for(Link current = first; current != null; current = current.getNext()) {
+//            result += current.getInfo();
+//        }
+//        return result;
+//    }
 
-    public int sum1() {
-        return sum2(first);
-    }
-
-    private int sum2(Link link) {
-        if (link == null) return 0;
-        return sum2(link.next) + link.info;
-    }
+//    public int sum1() {
+//        return sum2(first);
+//    }
+//
+//    private int sum2(Link link) {
+//        if (link == null) return 0;
+//        return sum2(link.next) + link.info;
+//    }
 }
--- Lists/src/Test.java
+++ Lists/src/Test.java
@@ -1,10 +1,27 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Test {
     public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
+        LinkedList<Integer> ll = new LinkedList<>();
         ll.add(12);
         ll.add(8);
         ll.add(2);
+        for (Integer i : ll) {
+            System.out.println("Элемент: " + i);
+        }
+        System.out.println(ll.size());
+        ll.remove(8);
         System.out.println(ll.size());
         System.out.println(ll); // [2, 8, 12] - StringBuilder (можно и рекурсивно, но сложнее)
+        LinkedList<String> ll1 = new LinkedList<>();
+        ll1.add("Hello");
+        for (String s : ll1) {
+
+        }
+        for (Iterator<String> iterator = ll1.iterator(); iterator.hasNext(); ) {
+            String s = iterator.next();
+
+        }
     }
 }
